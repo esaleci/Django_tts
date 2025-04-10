d5926690d5a863aa0e3d04a35408811d530d716e
el@ <40316929+esaleci@users.noreply.github.com>
2025-04-10 22:32:18 +0100

    last-v-1.15  vera

--- api/settings.py
+++ api/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
